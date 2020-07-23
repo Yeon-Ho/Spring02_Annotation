a78bde590aa6c8528a7f203da10ffe5da3d8f2ac
user2 <user2@user2-PC>
2020-07-23 19:03:16 +0900

    Annotation practice

---
+++ Spring02_Anno/src/test01/NickName.java
@@ -0,0 +1,16 @@
+package test01;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NickName {
+
+	@Override
+	public String toString() {
+		return "teacher";
+	}
+	
+	
+
+	
+}
